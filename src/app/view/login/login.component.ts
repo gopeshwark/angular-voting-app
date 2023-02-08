74872d0395d2a967791c8b1377f843ab904c8cd8
gopeshwark <gopeshwark@gmail.com>
2023-02-09 00:21:37 +0530

    Added tailwind and login page

---
+++ src/app/view/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['test@test.com', [Validators.required, Validators.email]],
+      password: ['12345', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  login() {
+    console.log(this.form.value);
+
+  }
+}
